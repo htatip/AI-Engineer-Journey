a0a3cdb1a17a4288f2508b670c0d010f6e8cf54e
Harshi <harshitsaitatiparthi@gmail.com>
2026-03-08 15:38:33 +0530

    Day 1

---
+++ Day 1/calculator.py
@@ -0,0 +1,8 @@
+#user Input
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
---
+++ Day 1/practice.py
@@ -0,0 +1,25 @@
+#Printing name
+name = input("Enter your name: Harshi")
+print(f'My name is {name}!')
+#Adding two numbers
+num1 = 10
+num2 = 20
+
+sum = num1 + num2
+
+print("Sum is:", sum)
+#converting temprature
+celsius = 25
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
+
+#Calculate Simple Intrest
+p = 1000   # principal
+r = 5      # rate
+t = 2      # time
+
+si = (p * r * t) / 100
+
+print("Simple Interest:", si)
